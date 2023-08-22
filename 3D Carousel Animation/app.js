8f697970319ed09c78489044155f543fdd4631f6
HackUniv <119047006+HackUniv@users.noreply.github.com>
2023-08-22 17:32:43 +0530

    Update app.js

--- 3D Carousel Animation/app.js
+++ 3D Carousel Animation/app.js
@@ -4,25 +4,24 @@ import {TiChevronLeftOutline, TiChevronRightOutline} from 'https://cdn.skypack.d
 const CARDS = 10;
 const MAX_VISIBILITY = 3;
 const images = [
-"https://github.com/HackUniv/Images/assets/119047006/9941ccf1-157c-4a67-ae6a-d75e84c70a85",
-"https://github.com/HackUniv/Images/assets/119047006/91567f4d-9c57-46d4-a527-eea24f21976b",
-"https://github.com/HackUniv/Images/assets/119047006/b256e3dd-2c25-4292-a9c5-4e5d610877dc",
-"https://github.com/HackUniv/Images/assets/119047006/94f6216c-e565-4740-88ce-df2c7531a003",
-"https://github.com/HackUniv/Images/assets/119047006/9b9a1293-8756-4e6e-a809-20de0a2f3e9a",
-"https://github.com/HackUniv/Images/assets/119047006/9a888ee2-8275-408c-8749-c68371512871",
-"https://github.com/HackUniv/Images/assets/119047006/10342278-5a87-4659-a56a-ab60abfcd3c8",
-"https://github.com/HackUniv/Images/assets/119047006/23f7f8b4-7a2d-4a5f-acdd-4f7e7af6e337",
-"https://github.com/HackUniv/Images/assets/119047006/26921037-3f4f-4c2b-9f2c-d061b03ef38b",
-"https://github.com/HackUniv/Images/assets/119047006/6d8802ae-15ff-409d-9297-c8554bcdeeac",
-"https://github.com/HackUniv/Images/assets/119047006/0cae3dbd-1db6-4916-89da-bc910967e162",
-"https://github.com/HackUniv/Images/assets/119047006/cab69b22-ff26-4917-aa3f-062235f330fe",
-"https://github.com/HackUniv/Images/assets/119047006/04671b01-9dad-4f21-a2a4-5ca4d92fe425",
-"https://github.com/HackUniv/Images/assets/119047006/85d4397d-c961-455c-95cd-1ebc93dc71e9"
+"https://github.com/HackUniv/Images/assets/119047006/9531fc14-e9ef-4e40-b7b0-7c6ba9008eb4",
+"https://github.com/HackUniv/Images/assets/119047006/25c8340e-df99-4c0c-b3ae-c2419d40ee90",
+"https://github.com/HackUniv/Images/assets/119047006/c9f8d6ae-2a16-415e-b79f-983757d6b611",
+"https://github.com/HackUniv/Images/assets/119047006/38a44d45-7de1-453f-a5f5-42c7cd1de191",
+"https://github.com/HackUniv/Images/assets/119047006/7aaf1da3-a561-4b4e-a26e-0d7470b29692",
+"https://github.com/HackUniv/Images/assets/119047006/65c126ba-3613-44f0-8e9c-f004c220fe86",
+"https://github.com/HackUniv/Images/assets/119047006/522e9dc6-4a79-4de2-84fb-4458675b542c",
+"https://github.com/HackUniv/Images/assets/119047006/68762ed6-3769-4ced-9f8e-d1ddc01e9174",
+"https://github.com/HackUniv/Images/assets/119047006/bc27de3b-1b8c-46b7-87ac-efb3e32d3efd",
+"https://github.com/HackUniv/Images/assets/119047006/bf5b78c4-539e-4bc0-8c50-2578b081d761",
+"https://github.com/HackUniv/Images/assets/119047006/7bca20ff-faf4-463e-9267-280e12dc954c",
+"https://github.com/HackUniv/Images/assets/119047006/f5ee4bbb-1e16-48d9-88f6-bbd52e8e4846",
+"https://github.com/HackUniv/Images/assets/119047006/c49d4d7c-57a8-4133-9226-568997d4c0b8",
 ]
 const Card = ({title, content}) => (
   <div className='card' style={{overflow:'hidden'}}>
     <h3 style={{fontSize : '20px' , textAlign:'center' , fontWeight: '900' , color:'black'}}>{title}</h3>
-    <img src ={content} style={{width:'19rem'}} />
+    <img src ={content} style={{width:'21rem'}} />
   </div>
 );
 const Carousel = ({children}) => {
